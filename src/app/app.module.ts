5a327804b1cff760d18363ecd590dcc2a1be6512
BanlyTong <banly2602@gmail.com>
2021-12-26 14:32:09 +0700

    chore: backup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -9,6 +10,7 @@ import { AppMaterialModule } from "./app-material/app-material.module";
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { DarkModeDirective } from './dark-mode.directive';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +18,15 @@ import { DarkModeDirective } from './dark-mode.directive';
     HeroesComponent,
     HeaderComponent,
     MenuComponent,
-    DarkModeDirective
+    DarkModeDirective,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppMaterialModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,9 +14,21 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
   
-  constructor() { }
+  heroes: Hero[];
+  selectedHero: Hero;
+  
+  
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
